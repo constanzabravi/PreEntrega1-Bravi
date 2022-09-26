1c4d2be2472081d710c20242fabed12b2d688c6e
CotiBravi <constanzabravidg@gmail.com>
2022-09-26 17:42:03 -0300

    primera pre entrega

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,44 +1,24 @@
-import './NavBar.css'
-// import logo from "./assets"
+import moto from '../CartWidget/assets/moto.png'
+import CartWidget from '../CartWidget/CartWidget'
+import './NavBar.css' 
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand text-white" href="#">ProgramAR</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarScroll">
-                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{'--bs-scroll-height': 100 + 'px'}}>
-                        <li className="nav-item">
-                            <a className="text-white nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="text-white nav-link" href="#">Nosotros</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="text-white nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Cursos
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Curso de Frontend Developer</a></li>
-                                <li><a className="dropdown-item" href="#">Curso de Backend Developer</a></li>
-                                <li><hr className="dropdown-divider"></hr></li>
-                                <li><a className="dropdown-item" href="#">Curso de Data Science</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="text-white nav-link disabled">Ayuda</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="search"></input>
-                        <button className="btn btn-dark" type="submit">Buscar</button>
-                    </form>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <img src={moto} alt='cart' />
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <button className="margin nav-item nav-link active">Home</button>
+                    <button className="margin nav-item nav-link">Nosotros</button>
+                    <button className="margin nav-item nav-link">Productos</button>
+                    <button className="margin nav-item nav-link">Contacto</button>
                 </div>
             </div>
-        </nav>
+            <CartWidget />
+        </nav >
     )
 }
 
