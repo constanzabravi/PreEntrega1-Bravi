f188a5a04f92b5db8a73ceeb498900685b17e4ba
CotiBravi <constanzabravidg@gmail.com>
2022-11-06 05:33:33 -0300

    refactorizacion de orders

---
+++ src/ServicesFirebase/firestore/orders.js
@@ -0,0 +1,69 @@
+import { collection, getDocs, query, where, documentId, writeBatch, addDoc, Timestamp } from 'firebase/firestore'
+import { db } from "../../ServicesFirebase/index"
+import { useContext } from "react"
+import { CartContext } from '../../CartContext/CartContext'
+
+export const useOrders = () => {
+    const { cart, totalQuantity } = useContext(CartContext)
+
+    const createOrder = async (orderData) => {
+        try {
+            const objOrder = {
+                buyer: orderData,
+                items: cart,
+                total: totalQuantity,
+                date: Timestamp.fromDate(new Date())
+            }
+
+
+            //Creo el Batch
+            const batch = writeBatch(db)
+            //Guardo productos fuera del stock en un array
+            //Si este array está vacio, todos los productos tienen el stock correcto
+            //Si tiene algun producto, significa que no tiene stock
+            const outOfStock = []
+            //Productos con los ids en el carrito
+            const ids = cart.map(prod => prod.id)
+            //Consulto en la base de datos esos productos
+            const productsRef = collection(db, 'productos')
+            //Consulto por los productos que únicamente están agregados al carrito, y con documentid los filtro
+            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
+            //Me traigo los docs actualizados de la base de datos
+            const { docs } = productsAddedFromFirestore
+            //Una vez que tengo esos productos los recorro
+            //Comparo el valor del stock de los productos con la cantidad de ese producto que agregó el usuario al carrito
+            docs.forEach(doc => {
+                //Con esta función obtengo el stock de los campos
+                const dataDoc = doc.data()
+                const stockDb = dataDoc.stock
+                //Obtengo la cantidad con el id que el usuario agregó (quantity) al carrito del producto, y lo encuentro en el carrito, trayendome la cantidad con el contador
+                const productAddedToCart = cart.find(prod => prod.id === doc.id)
+                const prodQuantity = productAddedToCart?.quantity
+                //Una vez que tengo los valores los comparo, si el valor que tengo en la base de datos es > o = a la cantidad que agrego el usuario, puede vender correctamente.
+                if (stockDb >= prodQuantity) {
+                    batch.update(doc.ref, { stock: stockDb - prodQuantity })
+                } else {
+                    //Todos los productos que pasen la validación se guardan en batch, los que no, en outofstock
+                    outOfStock.push({ id: doc.id, ...dataDoc })
+                }
+            })
+
+            if (outOfStock.length === 0) {
+                const ordersRef = collection(db, 'orders')
+                const orderAdded = await addDoc(ordersRef, objOrder)
+                batch.commit()
+                return { result: 'orderCreated', id: orderAdded.id }
+            } else {
+                return { result: 'outOfStockError', products: outOfStock }
+            }
+        } catch (error) {
+            return error
+        }
+    }
+
+    return {
+        createOrder
+    }
+}
+
+
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,11 +1,10 @@
 import { useState, useContext } from "react"
 import { CartContext } from "../../CartContext/CartContext"
-import { collection, getDocs, query, where, documentId, writeBatch, addDoc, Timestamp } from 'firebase/firestore'
-import { db } from "../../ServicesFirebase/index"
 import { useNavigate } from "react-router-dom"
 import FormularioCliente from '../Form/Form'
 import Swal from "sweetalert2";
 import './Checkout.css'
+import { useOrders } from "../../ServicesFirebase/firestore/orders"
 
 //Función para comprar y confirmar enviando datos a firebase
 const Checkout = () => {
@@ -20,105 +19,67 @@ const Checkout = () => {
         setPersonalData(true)
     }
 
-    const { cart, totalQuantity, clearCart } = useContext(CartContext)
+    const { clearCart } = useContext(CartContext)
 
     const navigate = useNavigate()
 
-    const createOrder = async () => {
+    const { createOrder } = useOrders()
+
+    const getOrder = () => {
+
         setLoading(true)
-        //Donde guardo los errores
-        try {
-            const objOrder = {
-                buyer: datosCompra,
-                items: cart,
-                total: totalQuantity,
-                date: Timestamp.fromDate(new Date())
-            }
-            //Creo el Batch
-            const batch = writeBatch(db)
-            //Guardo productos fuera del stock en un array
-            //Si este array está vacio, todos los productos tienen el stock correcto
-            //Si tiene algun producto, significa que no tiene stock
-            const outOfStock = []
-            //Productos con los ids en el carrito
-            const ids = cart.map(prod => prod.id)
-            //Consulto en la base de datos esos productos
-            const productsRef = collection(db, 'productos')
-            //Consulto por los productos que únicamente están agregados al carrito, y con documentid los filtro
-            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
-            //Me traigo los docs actualizados de la base de datos
-            const { docs } = productsAddedFromFirestore
-            //Una vez que tengo esos productos los recorro
-            //Comparo el valor del stock de los productos con la cantidad de ese producto que agregó el usuario al carrito
-            docs.forEach(doc => {
-                //Con esta función obtengo el stock de los campos
-                const dataDoc = doc.data()
-                const stockDb = dataDoc.stock
-                //Obtengo la cantidad con el id que el usuario agregó (quantity) al carrito del producto, y lo encuentro en el carrito, trayendome la cantidad con el contador
-                const productAddedToCart = cart.find(prod => prod.id === doc.id)
-                const prodQuantity = productAddedToCart?.quantity
-                //Una vez que tengo los valores los comparo, si el valor que tengo en la base de datos es > o = a la cantidad que agrego el usuario, puede vender correctamente.
-                if (stockDb >= prodQuantity) {
-                    batch.update(doc.ref, { stock: stockDb - prodQuantity })
-                } else {
-                    //Todos los productos que pasen la validación se guardan en batch, los que no, en outofstock
-                    outOfStock.push({ id: doc.id, ...dataDoc })
-                }
-            })
-
-            if (outOfStock.length === 0) {
-                await batch.commit()
-
-                const orderRef = collection(db, 'orders')
-
-                const orderAdded = await addDoc(orderRef, objOrder)
-
+        createOrder(datosCompra).then(response => {
+            if (response.result === 'orderCreated') {
                 clearCart()
-
                 setTimeout(() => {
                     navigate('/')
                 }, 2000)
                 Swal.fire({
                     title: "Gracias por su compra",
-                    text: `El id de su orden es: ${orderAdded.id}`,
+                    text: `El id de su orden es: ${response.id}`,
                     icon: "success",
                     buttons: true,
                     dangerMode: true,
                 })
-              
+
+                navigate('/')
             } else {
                 Swal.fire({
                     title: "Algunos productos no se encuentran en stock",
                     icon: "warning",
                     buttons: true,
                     dangerMode: true,
-
                 })
+                navigate('/cart')
             }
 
-        } catch (error) {
-            console.log(error)
-        } finally {
+        }).catch(error => {
+            Swal.fire({
+                title: `${error}`,
+                icon: false,
+                buttons: true,
+                dangerMode: true,
+                timer: 6000,
+                customClass: "swAlert"
+            })
+        }).finally(() => {
             setLoading(false)
-        }
-
+        })
     }
-
     if (loading) {
-        return <h1 className="center">Se esta procesando su pedido...</h1>
+        return (<h1 className="center">Se esta procesando su pedido...</h1>)
     }
-
     //Parte visual del componente lógico que deriva a generar pedido
 
     return (
         //Llamo al form con la funcion de completodatos que modificaba el state
         <div className="container text-center">
-        <div className=".col-md-6 .offset-md-3">
-            <FormularioCliente completoDatos={completoDatos} />
-            { personalData
-            ?<button className="confirmar animate__animated animate__backInDown" onClick={createOrder}>CONFIRMAR COMPRA</button>
-        : ""}
-        </div>
+            <div className=".col-md-6 .offset-md-3">
+                <FormularioCliente completoDatos={completoDatos} />
+                {personalData
+                    ? <button className="confirmar animate__animated animate__backInDown" onClick={getOrder}>CONFIRMAR COMPRA</button>
+                    : ""}
+            </div>
         </div>
     )
 }
